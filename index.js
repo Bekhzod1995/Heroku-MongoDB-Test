0bbb2d173621dfcea66ddc6058f84cc1611efdfe
Bekhzod1995 <81156beka@gmail.com>
2022-08-09 12:29:05 +0500

    test

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/test', (req, res) => {
   res.send('Hello test');
 });
 
+app.get('/working', (req, res) => {
+  res.send('yesss');
+});
+
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Example app listening on port ${port}`);
 });
